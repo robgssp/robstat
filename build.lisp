d3ddfcd203638093a1a5077d26aa826cc7570e93
Robert Glossop <robgssp@gmail.com>
2024-01-07 13:44:32 -0800

    asdf:program-op has some bizarre behavior, so don't use
    It sometimes outputs to the current directory and sometimes to its own
    build dir.

--- build.lisp
+++ build.lisp
@@ -3,6 +3,12 @@
 (require :asdf)
 (asdf:load-asd (merge-pathnames "robstat.asd" (uiop:getcwd)))
 (asdf:load-system :robstat)
-;; (sb-ext:save-lisp-and-die "robstat" :executable t :toplevel #'robstat:main)
-(asdf:operate :program-op "robstat")
-
+(sb-ext:save-lisp-and-die "robstat" :executable t :toplevel #'robstat:main)
+;; (uiop:delete-file-if-exists "robstat")
+;; (let* ((image
+;;          (asdf:output-file
+;;           (asdf:operate :program-op "robstat")
+;;           "robstat"))
+;;        (output (merge-pathnames "robstat" (uiop:getcwd))))
+;;   (format t "Copying ~a to ~a~%" image output)
+;;   (uiop:rename-file-overwriting-target image output))
